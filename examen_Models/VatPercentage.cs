ac2a0775abede4a94a13fa9f0777360449b1d039
lachiu <mathieu.van.daele@hotmail.com>
2023-08-30 23:11:27 +0200

    Added VatPercentage Model

---
+++ examen_Models/VatPercentage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace examen_models
+{
+	[Table("VatPercentages")]
+	public class VatPercentage : BaseModel
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public int Percentage { get; set; }
+		public List<TicketsProducts> TicketsProducts { get; set; }
+	}
+}

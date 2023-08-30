d5d737a15e2b542b5c4d04a73eac9b8c56e606f6
lachiu <mathieu.van.daele@hotmail.com>
2023-08-30 23:09:55 +0200

    Added location

---
+++ examen_Models/Locations.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace examen_models
+{
+	[Table("Locations")]
+	public class Location : BaseModel
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Display(Name = "Naam")]
+		[Required(ErrorMessage = "Het veld 'naam' moet ingevuld zijn.")]
+		[MaxLength(50)]
+		public string Name { get; set; }
+		public List<Stock> Stocks { get; set; }
+	}
+}

02cfb04801a411b68f1321106d16a0598382eb2f
lachiu <mathieu.van.daele@hotmail.com>
2023-08-30 23:10:10 +0200

    Added Stock

---
+++ examen_Models/Stock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Linq;
+
+namespace examen_models
+{
+	[Table("Stocks")]
+	public class Stock : BaseModel
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Display(Name = "Hoeveelheid")]
+		[Required(ErrorMessage = "Het veld 'hoeveelheid' moet ingevuld zijn.")]
+		public int Qty { get; set; }
+
+		[Required(ErrorMessage = "Een locatie moet geselecteerd zijn.")]
+		[ForeignKey("Location")]
+		public int LocationId { get; set; }
+
+		[Required(ErrorMessage = "Een product moet geselecteerd zijn.")]
+		[ForeignKey("Product")]
+		public int ProductId { get; set; }
+
+		public virtual Location Location { get; set; }
+		public virtual Product Product { get; set; }
+	}
+}

3891f0c58b90d64ec12e8242270f8630dffb50c7
lachiu <mathieu.van.daele@hotmail.com>
2023-08-30 23:10:37 +0200

    Added Product Model

---
+++ examen_Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using System.Xml.Linq;
+
+namespace examen_models
+{
+	[Table("Products")]
+	public class Product : BaseModel
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Display(Name = "Naam")]
+		[Required(ErrorMessage = "Het veld 'naam' moet ingevuld zijn.")]
+		[MaxLength(100)]
+		public string Name { get; set; }
+
+		[Display(Name = "Beschrijving")]
+		public string Description { get; set; }
+
+		[MaxLength(100)]
+		public string Barcode { get; set; }
+
+		public List<Stock> Stocks { get; set; }
+		public List<TicketsProducts> TicketsProducts { get; set; }
+	}
+}

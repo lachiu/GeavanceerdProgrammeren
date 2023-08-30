5004dace1ac483866cd9a33a70dedf9f55405efb
lachiu <mathieu.van.daele@hotmail.com>
2023-08-30 23:10:22 +0200

    Added Store Model

---
+++ examen_Models/Store.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Sockets;
+using System.Text;
+using System.Xml.Linq;
+
+namespace examen_models
+{
+	[Table("Stores")]
+	public class Store : BaseModel
+	{
+		[Key]
+		public int Id { get; set; }
+		[Display(Name = "Naam")]
+		[Required(ErrorMessage = "Het veld 'naam' moet ingevuld zijn.")]
+		[MaxLength(50)]
+		public string Name { get; set; }
+
+		public List<Ticket> Tickets { get; set; }
+	}
+}
